69792ee4b38efd61b9c2956a12e6060283971f2c
Suelaine Andrade <suelaine.andrade@gmail.com>
2018-08-20 14:23:17 -0300

    Rest Testado

---
+++ src/main/java/com/suelaine/cursomc2/resource/CategoriaResource.java
@@ -0,0 +1,17 @@
+package com.suelaine.cursomc2.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest Está Funcionando!";
+		
+	}
+
+}
