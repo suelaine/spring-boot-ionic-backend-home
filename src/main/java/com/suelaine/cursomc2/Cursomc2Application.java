d40cc7f6695a02786f68d264b70f86715aaf0c15
Suelaine Andrade <suelaine.andrade@gmail.com>
2018-08-20 18:00:34 -0300

    Criando Operação de Instanciação

--- src/main/java/com/suelaine/cursomc2/Cursomc2Application.java
+++ src/main/java/com/suelaine/cursomc2/Cursomc2Application.java
@@ -1,12 +1,31 @@
 package com.suelaine.cursomc2;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.suelaine.cursomc2.domain.Categoria;
+import com.suelaine.cursomc2.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Cursomc2Application {
+public class Cursomc2Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
+		
 	}
 }
