9969d8bc8fce1f93dd54331b628abf066192d6b6
Suelaine Andrade <suelaine.andrade@gmail.com>
2018-08-20 15:41:09 -0300

    Objetos

--- src/main/java/com/suelaine/cursomc2/resource/CategoriaResource.java
+++ src/main/java/com/suelaine/cursomc2/resource/CategoriaResource.java
@@ -1,17 +1,27 @@
 package com.suelaine.cursomc2.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.suelaine.cursomc2.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest Está Funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informãtica");
+		Categoria cat2 = new Categoria(2,"Escritório");
 		
+		List<Categoria> lista= new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
