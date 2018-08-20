5ad75ca980aea46d56fb8362e29cc66677c0119c
Suelaine Andrade <suelaine.andrade@gmail.com>
2018-08-20 14:05:00 -0300

    Projeto Criado

---
+++ src/main/java/com/suelaine/cursomc2/Cursomc2Application.java
@@ -0,0 +1,12 @@
+package com.suelaine.cursomc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc2Application.class, args);
+	}
+}
